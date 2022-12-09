f42704ab750b774e0910588e533b3bffc6035840
zhu3154 <zhu24314@outlook.com>
2022-12-09 21:33:44 +0800

    example program
    4.4
    4.5

---
+++ C primer plus/4.4 pizza.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define PI 3.14159
+int main(void)
+{
+	float area, circum, radius;
+
+	printf("What is the radius of your pizza?\n");
+	scanf_s("%f", &radius);
+	area = PI * radius * radius;
+	circum = 2.0 * PI * radius;
+	printf("Your basic pizza parameters are as follow:\n");
+	printf("circumference=%1.2f, area=%1.2f\n", circum, area);
+
+	return 0;
+}
