e4224c3f79d787b8c0bdcb034e49b344ed4e5a32
zhu3154 <zhu24314@outlook.com>
2022-12-10 11:15:07 +0800

    upload

--- C primer plus/exercise 4.4.c
+++ C primer plus/exercise 4.4.c
@@ -1,1 +1,19 @@
 #include<stdio.h>
+#define B "booboo"
+#define X 10
+int main(void)
+{
+	int age;
+	int xp;
+	char name[30];
+
+	printf("Pleaes enter your first name.");
+	scanf_s("%s", name, 30);
+	printf("All right,%s,what't your age?\n", name);
+	scanf_s("%d", &age);
+	xp = age + X;
+	printf("That's a %s!You must be at least %d.\n",
+		B, xp);
+
+	return 0;
+}
