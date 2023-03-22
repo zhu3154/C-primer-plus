9b9a18630d81c514278f88095ff091dee59a235a
zhu3154 <zhu24314@outlook.com>
2023-03-22 16:18:45 +0800

    upload

---
+++ C primer plus/Chapter 12/12.1 hiding.c
@@ -0,0 +1,24 @@
+// 块中的变量
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 30; // 原始的x
+
+    printf("x in outer block: %d at %p\n", x, &x);
+    {
+        int x = 77; // 新的x，隐藏了原始的x
+        printf("x in inner block: %d at %p\n", x, &x);
+    }
+    printf("x in outer block: %d at %p\n", x, &x);
+    while (x++ < 33) // 原始的x
+    {
+        int x = 100; // 新的x，隐藏了原始的x
+        x++;
+        printf("x in while loop: %d at %p\n", x, &x);
+    }
+    printf("x in outer block: %d at %p\n", x, &x);
+
+    getchar();
+    return 0;
+}
